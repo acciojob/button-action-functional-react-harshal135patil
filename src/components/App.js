3f0d441e7844afd258db361beb65adca8df36f67
Harshu135patil <harsh135235@gmail.com>
2022-12-16 22:11:22 +0530

    Done using hook

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const[show, setShow] = useState('')
   return (
     <div id="main">
-      // Do not alter the main div
-    </div>
+     <button> id="click" onClick={()=> setShow("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")} </button>
+    <p id="para">{show}</p>
+    </div> 
   );
 }
 
